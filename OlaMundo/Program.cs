54cd52405ad6c7e6b5b8878d900e5b52042fc211
cupstealer <guisilvanegrinho@gmail.com>
2023-03-03 11:38:03 +0000

    Actually adding a comment now

--- OlaMundo/Program.cs
+++ OlaMundo/Program.cs
@@ -2,8 +2,10 @@
 
 namespace OlaMundo
 {
+    ///when the program is run, it display the text inside "Console.WRITE.Line" command (arguments)
     class Program
     {
+    
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
