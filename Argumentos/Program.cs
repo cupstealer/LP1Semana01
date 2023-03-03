44391de0324a848b4f451b3fce33ce5ab298172e
cupstealer <guisilvanegrinho@gmail.com>
2023-03-03 11:36:58 +0000

    Adding a comment

---
+++ Argumentos/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+include <stdio.h>;
+include <conio.h>;
+
+namespace Argumentos
+{
+    class Program
+    {
+        //Command here: http://linguagemc.com.br/argumentos-em-linha-de-comando/
+        
+        int main( int argc, char *argv)
+
+        {
+            int cont;
+            
+            for(cont=0; cont < argc; cont++)
+                object value = printf("%d Argumentos: %s\n", cont,argv[cont]);
+
+        }
+    }
+}
